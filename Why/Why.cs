1f5c2d545c2712717d5fbea7bc316adef0e4b383
unknown <radyoczlonek@gmail.com>
2021-05-18 18:20:07 +0200

    Day 1?

---
+++ Why/Why.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Why
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Why?";
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WindowHeight = 20;
+
+            Console.WriteLine("Why are you looking at those files?");
+            Console.ReadLine();
+            Console.WriteLine("Yea whatever \nfuck off");
+            Console.WriteLine("Why are you still here?");
+            Console.ReadLine();
+            Console.WriteLine("Cool motive \nStill murder");
+            Console.ReadKey();
+        }
+    }
+}
