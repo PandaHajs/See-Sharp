99f1098aca3852886c81645119d99a39ee91e8e6
unknown <radyoczlonek@gmail.com>
2021-05-19 19:17:23 +0200

    day 2?

--- Conditions 4/Conditions 4.cs
+++ Conditions 4/Conditions 4.cs
@@ -6,7 +6,31 @@ namespace Conditions_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input a number between 1 and 5: ");
+            int num = Convert.ToInt32( Console.ReadLine() );
+
+            switch (num){
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                default:
+                    Console.WriteLine("That's not between 1 and 5");
+                    break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
