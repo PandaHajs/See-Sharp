57248ee314c188b420b9ca71e012590a695b2d59
unknown <radyoczlonek@gmail.com>
2021-05-20 15:49:48 +0200

    day 3?

---
+++ Arrays 2/Arrays 2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>();
+
+            shoppingList.Add("Dreams");
+            shoppingList.Add("Miracles");
+            shoppingList.Add("Rainbows");
+            shoppingList.Add("Pony");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
+            shoppingList.Remove("Dreams");
+            shoppingList.RemoveAt(1);
+
+            Console.WriteLine("----------------");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
