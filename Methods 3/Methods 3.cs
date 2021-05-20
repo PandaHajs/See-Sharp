450650cfc4cc3c8fdf334e0059f22fd929123128
unknown <radyoczlonek@gmail.com>
2021-05-20 17:53:38 +0200

    day 3? End of the course with Brackeys ;-;

---
+++ Methods 3/Methods 3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wordc();
+
+            
+            
+            Console.ReadKey();
+        }
+
+        static void wordc(){
+
+            Console.Write("Input your sentence: ");
+            string sent = Convert.ToString(Console.ReadLine());
+            
+            int lenght = Convert.ToInt32(sent.Split(' ').Length);
+
+            Console.WriteLine("This sentence has " + lenght + " words");
+        }
+    }
+}
